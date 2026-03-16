04489764d2f171b4c1ddcc59f7ced00d32a1963e
Willame <seu-email-no-github@example.com>
2026-03-15 22:04:22 -0300

    fix rota raiz render

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const cors = require('cors');
 const db = require('./config/db');
 
-// Importa a nossa nova rota
 const importacaoRoutes = require('./routes/importacao'); 
 const dashboardRoutes = require('./routes/dashboard');
 const acidentesRoutes = require('./routes/acidentes');
@@ -12,10 +11,16 @@ const authRoutes = require('./routes/auth');
 const adminRoutes = require('./routes/admin');
 
 const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-// Diz ao servidor para usar a rota de importação quando o endereço tiver "/api"
+// rota raiz
+app.get('/', (req, res) => {
+  res.send('API Gestão SST rodando no Render 🚀');
+});
+
+// rotas da API
 app.use('/api', importacaoRoutes);
 app.use('/api/dashboard', dashboardRoutes);
 app.use('/api/acidentes', acidentesRoutes);
@@ -24,7 +29,8 @@ app.use('/api/absenteismo', absenteismoRoutes);
 app.use('/api/auth', authRoutes);
 app.use('/api/admin', adminRoutes);
 
+const PORT = process.env.PORT || 3000;
 
-app.listen(3000, () => {
-    console.log('Servidor rodando na porta 3000');
+app.listen(PORT, () => {
+  console.log(`Servidor rodando na porta ${PORT}`);
 });
